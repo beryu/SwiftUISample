8c9542a54fab57a7a5c5d4655880488d8a4ff1c4
Ryuta Kibe <ryutakibe@gmail.com>
2023-07-24 05:45:56 +0900

    feat: Support dark mode

--- Modules/Common/Sources/SharedExtension/Extension.View.swift
+++ Modules/Common/Sources/SharedExtension/Extension.View.swift
@@ -1,28 +1,29 @@
+import SharedResource
 import SwiftUI
 
 extension View {
   public func pageTitleText() -> some View {
     font(.system(size: 25, weight: .semibold))
-      .foregroundColor(Color.black)
+      .foregroundColor(Color(asset: Asset.text))
   }
 
   public func pageSubtitleText() -> some View {
     font(.system(size: 19, weight: .regular))
-      .foregroundColor(Color.black)
+      .foregroundColor(Color(asset: Asset.text))
   }
 
   public func cellTitleText() -> some View {
     font(.system(size: 19, weight: .semibold))
-      .foregroundColor(Color.black)
+      .foregroundColor(Color(asset: Asset.text))
   }
 
   public func bodyText() -> some View {
     font(.system(size: 14, weight: .regular))
-      .foregroundColor(Color.black)
+      .foregroundColor(Color(asset: Asset.text))
   }
 
   public func bodyBoldText() -> some View {
     font(.system(size: 14, weight: .semibold))
-      .foregroundColor(Color.black)
+      .foregroundColor(Color(asset: Asset.text))
   }
 }
